c2c6faab01830d667b81e9c45beb550cc778ca2d
Tom Hudson <tom.hudson.gamedev@gmail.com>
2020-07-20 21:05:33 +0100

    More tests
    A couple more tests in the bank account scenario have been added to check functionality is working as expected

--- Test-Practice/Test-Practice/test.cpp
+++ Test-Practice/Test-Practice/test.cpp
@@ -12,12 +12,35 @@ struct BankAccount
 	{
 
 	}
+
+	void deposit(const int n)
+	{
+		balance += n;
+	}
 };
 
-TEST(AccountTest, BankAccountStartsEmpty)
+struct BankAccountTest :testing::Test
 {
-	BankAccount account;
-	EXPECT_EQ(0, account.balance);
+	BankAccount* account;//setting up a local bank account to avoid creating a copy in every test case
+	BankAccountTest()
+	{
+		account = new BankAccount;
+	}
+	~BankAccountTest()
+	{
+		delete account;
+	}
+};
+
+TEST_F(BankAccountTest, BankAccountStartsEmpty) //basic test to see if the balance has been initialised to a default value of 0
+{
+	
+	EXPECT_EQ(0, account->balance); //checking if what we expect is true
+}
+TEST_F(BankAccountTest, CanDepositMoney)
+{
+	account->deposit(100);
+	EXPECT_EQ(100, account->balance);
 }
 
 int main(int argc, char* argv[])
