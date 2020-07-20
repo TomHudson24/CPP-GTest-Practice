4633b902d61270b464ce96eb236ad88c025c8e06
Tom Hudson <tom.hudson.gamedev@gmail.com>
2020-07-20 21:15:27 +0100

    Parameterized test added
    Added a test that allows me to query the functions with multiple account states to check if they are all working as intended.

--- Test-Practice/Test-Practice/test.cpp
+++ Test-Practice/Test-Practice/test.cpp
@@ -17,6 +17,16 @@ struct BankAccount
 	{
 		balance += n;
 	}
+
+	bool withdraw(const int amount)
+	{
+		if (amount <= balance)
+		{
+			balance -= amount;
+			return true;
+		}
+		return false;
+	}
 };
 
 struct BankAccountTest :testing::Test
@@ -26,7 +36,7 @@ struct BankAccountTest :testing::Test
 	{
 		account = new BankAccount;
 	}
-	~BankAccountTest()
+	virtual ~BankAccountTest()
 	{
 		delete account;
 	}
@@ -42,6 +52,34 @@ TEST_F(BankAccountTest, CanDepositMoney)
 	account->deposit(100);
 	EXPECT_EQ(100, account->balance);
 }
+
+struct account_state
+{
+	int init_balance;
+	int withdraw_amount;
+	int final_balance;
+	bool success;
+};
+
+struct WithdrawAccountTest : BankAccountTest, testing::WithParamInterface<account_state> //parametirized test case
+{
+	WithdrawAccountTest()
+	{
+		account->balance = GetParam().init_balance;
+	}
+};
+
+TEST_P(WithdrawAccountTest, FinalBalance)
+{
+	auto as = GetParam();
+	auto success = account->withdraw(as.withdraw_amount);
+	EXPECT_EQ(as.final_balance, account->balance);//does the final balance equal the right amount in the account
+	EXPECT_EQ(as.success, success); 
+}
+
+INSTANTIATE_TEST_CASE_P(default, WithdrawAccountTest, testing::Values(account_state{ 100,50,50,true }, account_state{ 100,200,100,false })); 
+//first account state should allow you to start with 100 then withdraw 50, be left with 50 and the success should be set to true.
+//second account state should start the balance at 100 then upon requesting to withdraw 200 this should fail and the account balance should be left as 100.
 
 int main(int argc, char* argv[])
 {
