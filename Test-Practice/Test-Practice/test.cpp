ecb9b59578b0c48837506a1883b131015a0d58c2
Tom Hudson <tom.hudson.gamedev@gmail.com>
2020-07-20 19:59:41 +0100

    First Test Done
    Written out a basic test to check if there is a default value in the constuctor of "Bank Account"

---
+++ Test-Practice/Test-Practice/test.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+struct BankAccount
+{
+	int balance = 0;
+
+	BankAccount()
+	{
+		//DEFAULT CONSTRUCTOR
+	}
+
+	explicit BankAccount(const int balance) : balance{ balance }
+	{
+
+	}
+};
+
+TEST(AccountTest, BankAccountStartsEmpty)
+{
+	BankAccount account;
+	EXPECT_EQ(0, account.balance);
+}
+
+int main(int argc, char* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
